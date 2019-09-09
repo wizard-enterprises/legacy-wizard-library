72b2990c186d06d3d552ce164e3321c1da0fb129
Eden <eden@wizard.enterprises>
2019-09-09 19:05:38 +0300

    Implement only flag

--- packages/polymorphic-tests/test_method.spec.ts
+++ packages/polymorphic-tests/test_method.spec.ts
@@ -28,24 +28,44 @@ abstract class TestEntity {
   get id(): string {
     return [
       this.type,
-      this.parentSuite
-        ? `_${this.parentSuite.id}_`
-        : null, this.name
+      ...(this.parentSuite
+        ? [
+          `_${this.parentSuite.id}_`,
+          this.parentSuite.subTestEntities.findIndex(sub => sub === this),
+        ]
+        : [null]),
+      this.name,
       ].filter(Boolean).join('_') 
   }
   public name: string
-  public parentSuite: TestSuite = null
+  public parentSuite: TestSuite
   public opts: TestEntityOpts
   private _status: TestEntityStatus = TestEntityStatus.pending
   public get status() { return this._status}
 
-  constructor(name, opts: TestEntityOpts) {
+  private _shouldSkipBecauseOfOnly = false
+  protected get shouldSkipBecauseOfOnly() { return this._shouldSkipBecauseOfOnly }
+  protected set shouldSkipBecauseOfOnly(shouldSkipBecauseOfOnly) {
+    this._shouldSkipBecauseOfOnly = shouldSkipBecauseOfOnly
+    this.updateForSkipBecauseOfOnly()
+  }
+
+  protected updateForSkipBecauseOfOnly() {}
+
+  constructor(name, opts: TestSuiteOpts | TestMethodOpts) {
     this.name = name
     this.opts = opts
+    if (!this.opts['rootSuite'])
+      this.parentSuite = GlobalTestSuite.getInstance()
   }
 
   public async run(reporter: TestReporterDelegate) {
-    if (this.opts.skip) return reporter.testEntitySkipped(this)
+    this.shouldSkipBecauseOfOnly = this.doesEntityHaveSubentitiesWithOnly(this) || this.opts.skipBecauseOfOnly
+    if (this.shouldSkipEntity(this)) {
+      reporter.testEntitySkipped(this)
+      if (this.type === TestEntityType.suite) await this.runTestEntity(reporter)
+      return
+    }
     reporter.testEntityIsExecuting(this)
     try {
       await this.runTestEntity(reporter)
@@ -54,6 +74,16 @@ abstract class TestEntity {
       let reasons = this.failureReasonsOverride.length ? this.failureReasonsOverride : [e]
       reporter.testEntityFailed(this, ...reasons)
     }
+  }
+
+  private doesEntityHaveSubentitiesWithOnly(entity: TestEntity) {
+    if (entity.type === TestEntityType.test) return false
+    let e: TestSuite = entity as TestSuite
+    return !!(e.subTestEntities.find(entity => entity.opts.only))
+  }
+
+  private shouldSkipEntity(entity: TestEntity) {
+    return entity.opts.skip || entity.opts.skipBecauseOfOnly
   }
 
   protected failureReasonsOverride: Error[] = []
@@ -66,25 +96,59 @@ abstract class TestEntity {
 }
 
 interface TestEntityOpts {
-  skip?: true
+  skip?: boolean,
+  skipBecauseOfOnly?: boolean,
+  only?: true,
 }
 
-interface TestSuiteOpts extends TestEntityOpts {}
+interface TestSuiteOpts extends TestEntityOpts {
+  rootSuite?: true
+}
 interface TestMethodOpts extends TestEntityOpts {}
 
 class TestSuite extends TestEntity {
   public type = TestEntityType.suite
-  public opts: TestSuiteOpts
-  subTestEntities: Array<TestEntity> = []
+  public subTestEntities: TestEntity[] = []
+  private _opts: TestSuiteOpts
+  public get opts(): TestSuiteOpts { return this._opts }
+  public set opts(opts: TestSuiteOpts) {
+    this._opts = opts
+    if (this.subTestEntities)
+      this.applyOptsToSubEntities(this.subTestEntities)
+  }
+  protected updateForSkipBecauseOfOnly() {
+    this.applyOptsToSubEntities(this.subTestEntities)
+  }
   constructor(name, opts: TestSuiteOpts = {}) {
     super(name, opts)
   }
 
   addSubTestEntities(...testEntities: TestEntity[]) {
-    for (let entity of testEntities)
-      entity.parentSuite = this
-    this.subTestEntities = this.subTestEntities.concat(testEntities)
+    this.subTestEntities = this.subTestEntities.concat(
+      this.applyOptsToSubEntities(testEntities))
     return this
+  }
+  
+  private applyOptsToSubEntities(entities: TestEntity[], opts: TestSuiteOpts = this.opts) {
+    for (let entity of entities) {
+      entity.parentSuite = this
+      opts = {...opts}
+      delete opts.rootSuite
+      delete opts.only 
+      let entityOpts = {
+        ...entity.opts,
+        skipBecauseOfOnly: this.shouldApplySkipBecauseOfOnlyToSubEntity(
+          entity, {...entity.opts, ...opts},
+        ),
+      }
+      entity.opts = entityOpts
+    }
+    return entities
+  }
+
+  private shouldApplySkipBecauseOfOnlyToSubEntity(entity, entityOpts) {
+    return !entity.opts.only && entityOpts.skipBecauseOfOnly ||
+      this.shouldSkipBecauseOfOnly && (!entityOpts.only || entityOpts.skip)
   }
 
   async runTestEntity(reporter: TestReporterDelegate) {
@@ -94,13 +158,19 @@ class TestSuite extends TestEntity {
 }
 
 class GlobalTestSuite extends TestSuite {
-  private instance = null
-  constructor(opts: any = {}) {
-    super('global', opts)
-    if (this.instance) return this.instance
-    this.parentSuite = null
-    this.instance = this
-    return this.instance
+  private static instance = null
+  static getInstance(opts: TestSuiteOpts = {}) {
+    return this.instance || (this.instance = new GlobalTestSuite(opts))
+  }
+
+  protected constructor(opts: TestSuiteOpts = {}) {
+    super('global', {...opts, rootSuite: true})//, only: true})
+  }
+}
+
+class GlobalTestSuiteForTests extends GlobalTestSuite {
+  static getNewInstance(opts: TestSuiteOpts = {}) {
+    return new GlobalTestSuite(opts)
   }
 }
 
@@ -121,7 +191,6 @@ class TestMethod extends TestEntity {
 class AssertionError extends Error {
   constructor(...args) {
     super(...args)
-    //@ts-ignore
     if (Error.captureStackTrace) Error.captureStackTrace(this, AssertionError)
   }
 }
@@ -140,6 +209,10 @@ function assertPrimitiveEqual(a, b, message?) {
 
 function assertIdentical(a, b, message?) {
   assert(a === b, message || `Expected "${a}" to be identical to "${b}"`)
+}
+
+function assertNot(boolean, message?) {
+  assert(!boolean, message || `Expected "${boolean}" to be falsey`)
 }
 
 function assert(boolean, message?) {
@@ -231,7 +304,7 @@ class SimpleTestReporter extends TestReporter {
   }
 
   async start() {
-    this.console.log('Running tests...\n')
+    this.console.log('Running tests...')
   }
   
   async end() {
@@ -267,51 +340,91 @@ async function getConsoleCallsFromRunningSuite(rootSuite: TestSuite) {
   return consoleSpy.calls
 }
 
-let suite = new TestSuite('simple reporter').addSubTestEntities(
-  new TestMethod('report no tests', async () => {
-    assertPrimitiveEqual(
-      (await getConsoleCallsFromRunningSuite(
-        new TestSuite('test'),
-      )).args().map(args => args[0]),
-      ['Running tests...\n', 'Run successful, 0/0 passed.'],
-    )
-  }),
-  
-  new TestMethod('report one skipped test', async () => {
-    assertPrimitiveEqual(
-      (await getConsoleCallsFromRunningSuite(
-        new TestSuite('test').addSubTestEntities(
-          new TestMethod('skip', () => assert(false), {skip: true}),
-        ),
-      )).args().map(args => args[0]),
-      ['Running tests...\n', 'Run successful, 0/1 passed.'],
-    )
-  }),
-  
-  new TestMethod('report one passing test', async () => {
-    assertPrimitiveEqual(
-      (await getConsoleCallsFromRunningSuite(
-        new TestSuite('test').addSubTestEntities(
-          new TestMethod('pass', () => assert(true)),
-        ),
-      )).args().map(args => args[0]),
-      ['Running tests...\n', 'Run successful, 1/1 passed.'],
-    )
-  }),
+GlobalTestSuite.getInstance().addSubTestEntities(
+  new TestSuite('simple flows').addSubTestEntities(
+    new TestMethod('report no tests', async () => {
+      assertPrimitiveEqual(
+        (await getConsoleCallsFromRunningSuite(
+          GlobalTestSuiteForTests.getNewInstance(),
+        )).args().map(args => args[0]),
+        ['Running tests...', 'Run successful, 0/0 passed.'],
+      )
+    }),
+    
+    new TestMethod('report one skipped test', async () => {
+      assertPrimitiveEqual(
+        (await getConsoleCallsFromRunningSuite(
+          GlobalTestSuiteForTests.getNewInstance().addSubTestEntities(
+            new TestMethod('skip', () => assert(false), {skip: true}),
+          ),
+        )).args().map(args => args[0]),
+        ['Running tests...', 'Run successful, 0/1 passed.'],
+      )
+    }),
+    
+    new TestMethod('report one passing test', async () => {
+      assertPrimitiveEqual(
+        (await getConsoleCallsFromRunningSuite(
+          GlobalTestSuiteForTests.getNewInstance().addSubTestEntities(
+            new TestMethod('pass', () => assert(true)),
+          ),
+        )).args().map(args => args[0]),
+        ['Running tests...', 'Run successful, 1/1 passed.'],
+      )
+    }),
 
-  new TestMethod('report failing suite', async () => {
-    let callArgs = (await getConsoleCallsFromRunningSuite(
-      new TestSuite('test').addSubTestEntities(
-        new TestMethod('pass', () => assert(true)),
-        new TestMethod('skip', () => assert(false), {skip: true}),
-        new TestMethod('failing', () => assert(false)),
-      ),
-    )).args()
-    assertIdentical(callArgs[0][0], 'Running tests...\n')
-    assertIncludes(callArgs[1].join('\n'), 'Test "failing" failed:')
-    assertIncludes(callArgs[1].join('\n'), 'Expected "false" to be truthy')
-    assertIdentical(callArgs[2][0], 'Run failed, 1 failed, 1/3 passed.')
-  })
+    new TestMethod('report failing suite', async () => {
+      let callArgs = (await getConsoleCallsFromRunningSuite(
+        GlobalTestSuiteForTests.getNewInstance().addSubTestEntities(
+          new TestMethod('pass', () => assert(true)),
+          new TestMethod('skip', () => assert(false), {skip: true}),
+          new TestMethod('failing', () => assert(false)),
+        ),
+      )).args()
+      assertIdentical(callArgs[0][0], 'Running tests...')
+      assertIncludes(callArgs[1].join('\n'), 'Test "failing" failed:')
+      assertIncludes(callArgs[1].join('\n'), 'Expected "false" to be truthy')
+      assertIdentical(callArgs[2][0], 'Run failed, 1 failed, 1/3 passed.')
+    }),
+
+    new TestMethod('report with only and skip', async () => {
+      let runCount = 0,
+        passingTest = () => runCount++,
+        skipOpts: TestEntityOpts = {skip: true},
+        onlyOpts: TestEntityOpts = {only: true, skip: false},
+        skipAndOnlyOpts: TestEntityOpts = {only: true, skip: true}
+      let calls = await getConsoleCallsFromRunningSuite(
+        GlobalTestSuiteForTests.getNewInstance().addSubTestEntities(
+          new TestSuite('should be skipped', skipAndOnlyOpts).addSubTestEntities(
+            new TestMethod('should be skipped', () => assert(false)),
+            new TestMethod('should be skipped', () => assert(false), onlyOpts),
+            ),
+          new TestSuite('should be skipped', skipOpts).addSubTestEntities(
+            new TestMethod('should be skipped', () => assert(false)),
+            new TestMethod('should be skipped', () => assert(false), onlyOpts),
+          ),
+          new TestSuite('should not run').addSubTestEntities(
+            new TestMethod('should not run', () => assert(false)),
+            new TestMethod('should run', passingTest, onlyOpts),
+          ),
+          new TestSuite('should run', onlyOpts).addSubTestEntities(
+            new TestMethod('should be skipped', () => assert(false), skipOpts),
+            new TestMethod('should run', passingTest),
+            new TestSuite('should not run').addSubTestEntities(
+              new TestMethod('should run', passingTest, onlyOpts),
+            ),
+          ),
+          new TestSuite('should run', onlyOpts).addSubTestEntities(
+            new TestMethod('should run', passingTest, onlyOpts),
+            new TestMethod('should not run', () => assert(false)),
+          ),
+        ),
+      )
+      assertIdentical(calls.args()[calls.args().length - 1][0], 'Run successful, 4/11 passed.')
+      assertIdentical(runCount, 4)
+    })
+  )
 )
 
-new TestRunner(suite, new SimpleTestReporter(suite)).run()
+let global = GlobalTestSuite.getInstance()
+new TestRunner(global, new SimpleTestReporter(global)).run()
