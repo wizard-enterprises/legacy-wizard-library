0eab08d9d7df9f60ae928d217c29d32b83c8e511
Eden <eden@wizard.enterprises>
2019-11-19 16:02:15 +0200

    Delete wizard-utils lib from git

--- packages/wizard-utils/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function makeStringEnum<T extends string = string>(...members: string[]): {
-    T: T;
-};
-export declare function jsonStringifyWithEscapedCircularRefs(obj: Object): string;
--- packages/wizard-utils/lib/puppeteer/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from '../';
-export declare function makeWindowErrorSerializable(e: any): Error;
-export declare function makeFunctionFromStringified(funcString: any): Function;
