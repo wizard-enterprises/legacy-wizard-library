d1190c818400d55fa58a5cac27868fff16d0a0d9
Eden <eden@wizard.enterprises>
2019-12-31 18:13:38 +0200

    wizard-components#form: Add choices.js

--- packages/wizard-components/src/form/index.ts
+++ packages/wizard-components/src/form/index.ts
@@ -1,4 +1,5 @@
 import 'formiojs/dist/formio.full.min.js'
+import 'choices.js/public/assets/scripts/choices.min'
 import { FormBuilder, Formio } from 'formiojs'
 import { customElement, property } from 'lit-element'
 import { CachedReturn } from 'wizard-decorators'
