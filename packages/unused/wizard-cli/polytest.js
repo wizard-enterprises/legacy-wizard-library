8a4b4ed39c0b61717676df3a1ec45944020f8bdd
Eden <eden@wizard.enterprises>
2020-04-26 19:18:26 +0300

    Remove old experiment

--- packages/unused/wizard-cli/polytest.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  tests: ['test/**/*.spec.js'],
-  codes: ['src/**/*.js', '!src/**/*.spec.js'],
-  reporter: 'mochaSpec',
-  setup: () => {
-    require('@babel/register')()
-  },
-}
