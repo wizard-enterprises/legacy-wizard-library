f9f575abc17e1381b451bec43bca2b428bc373df
Eden <eden@wizard.enterprises>
2021-10-27 17:24:43 +0300

    remove wip pipeline component

--- packages/wizard-components/pipeline/src/abstract/types.ts
+++
@@ -1,6 +0,0 @@
-export enum PipelineElementIOType {
-  inMemory = 'inMemory',
-  localStorage = 'localStorage',
-  sessionStorage = 'sessionStorage',
-  queryParams = 'queryParams',
-}
--- packages/wizard-components/pipeline/src/component/for-tests.ts
+++
@@ -1,8 +0,0 @@
-import { customElement } from 'lit-element'
-import { PipelineElement } from '.'
-import '../pipes'
-
-@customElement('wizard-pipeline-for-tests')
-export class PipelineElementForTests<inputT = any, outputT = inputT> extends PipelineElement<inputT, outputT> {
-  public getPipeSlot() { return this.pipeSlot }
-}
